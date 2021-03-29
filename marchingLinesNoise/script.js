dd2e26b1b0d5783bbebdc730664f4fb043717903
unknown <pierrick@live.co.uk>
2021-03-29 17:26:15 +0100

    Code refactor for background! Much better!!!!!

--- marchingLinesNoise/script.js
+++ marchingLinesNoise/script.js
@@ -1,37 +1,63 @@
-const points = []
-const speed = 0.001
+const DENSITY = 50
+let lines
 
 function setup() {
     createCanvas(window.innerWidth, window.innerHeight)
-    background(20)
+    background(0)
     angleMode(DEGREES)
-    noiseDetail(3)
-
-    var density = 50
-    var spacing = window.innerWidth / density
-
-    // Create a vector for each of the x, y coords
-    for (var x = 0; x < window.innerWidth; x += spacing) {
-        for (var y = 0; y < window.innerHeight; y += spacing) {
-            var point = createVector(x + random(-10, 10), y + random(-10, 10))
-            points.push(point)
-        }
-    }
+    noiseDetail(2)
+    lines = setupLines(DENSITY)
+    
 }
 
 function draw() {
     noStroke()
     fill(255)
 
-    for (var i = 0; i < points.length; i++) {
-        
+    lines.forEach(line => line.render())
+}
+
+function setupLines(density) {
+    const lines = []
+
+    const spacing = window.innerWidth / density
+    
+    for (let x = 0; x < window.innerWidth; x += spacing) {
+        for (let y = 0; y < window.innerHeight; y += spacing) {
+            const line = new Line(x, y)
+            lines.push(line)
+        }
+    }
+
+    return lines
+}
+
+class Line {
+    constructor(x, y) {
+        this.curveSpeed = 0.001
+        this.points = []
+        this.startX = x
+        this.startY = y
+        this.initialVector = createVector(x + random(-1, 1), y + random(-1, 1))
+        this.currentVector = this.initialVector.copy()
+    }
+
+    nextPoint() {
         // Angle at which each point moves towards
-        var angle = map(noise(points[i].x * speed, points[i].y * speed), 0, 1, 0, 720)
+        const angle = map(noise(this.currentVector.x * this.curveSpeed, this.currentVector.y * this.curveSpeed), 0, 1, 0, 720)
 
         // Add vector to each point based on angle variable
-        points[i].add(createVector(cos(angle), sin(angle)))
-        
-        // Ellipse at the x, y coords of each point
-        ellipse(points[i].x, points[i].y, 1)
+        this.currentVector.add(createVector(cos(angle), sin(angle)))
+
+        // Get a point using the current x, y of the vector
+        const point = {x: this.currentVector.x, y: this.currentVector.y}
+        this.points.push(point)
+
+        return point
+    }
+
+    render() {
+        const point = this.nextPoint()
+        ellipse(point.x, point.y, 1)
     }
 }
